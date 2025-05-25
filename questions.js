e8de1937faab76faa516a4b9669d3aac361303b8
kraksyx <blahblahusdme@gmail.com>
2025-05-25 19:51:08 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,302 @@
+const questions = [
+  {
+    "question": "The maximum theoretical range at which an aircraft at FL80 can obtain bearings from a ground VDF facility sited 325 FT above MSL is:",
+    "options": [
+      "107 NM",
+      "158 NM",
+      "134 NM",
+      "114 NM"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "What is the maximum range at which a VDF station at 325 ft can provide a service to an aircraft at FL080?",
+    "options": [
+      "134 nm",
+      "107 nm",
+      "91 nm",
+      "114 nm"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "When conducting a QGH approach, responsibility for interpreting the procedure rests with ___, and on a VDF approach responsibility rests with ___:",
+    "options": [
+      "The controller; the controller",
+      "The pilot; the pilot",
+      "The pilot; the controller",
+      "The controller; the pilot"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "If, when you are requesting a QDM from an airfield, you are offered a QGH, it means:",
+    "options": [
+      "The bearing will only be accurate when the aircraft is flying above the QGH level",
+      "The VDF service will be handled by a different VDF unit, operating on the same frequency",
+      "The VDF unit is prepared to give you assistance during an approach to the airfield, based on VDF bearings",
+      "The service will be limited to bearings, no positions will be given by the DF station"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "Ground DME responders respond at a frequency:",
+    "options": [
+      "The same as the interrogation signal",
+      "63 MHz greater than interrogation frequency",
+      "63 MHz lower than interrogation frequency",
+      "63 MHz different from interrogation frequency, either above or below"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "The phase difference between the reference and variable signals on QDM 050\u00b0 (VAR 100W) for a conventional VOR is:",
+    "options": [
+      "050\u00b0",
+      "040\u00b0",
+      "230\u00b0",
+      "220\u00b0"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "A VDF bearing can be obtained:",
+    "options": [
+      "Only on a frequency of 121.5 MHz",
+      "On the Approach frequency",
+      "On the frequency notified for VDF services",
+      "On the frequency agreed between the pilot and ATC but chosen from one of the available ATC frequencies"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "Which of the following does NOT affect the accuracy of VDF bearings:",
+    "options": [
+      "Sky waves",
+      "Duct propagation",
+      "Ground reflections",
+      "Synchronous transmissions"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "The range at which you can obtain a VDF bearing can be influenced by:",
+    "options": [
+      "Time of day",
+      "Type of surface",
+      "Height of aircraft",
+      "Intensity of ionisation"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "At a height of 5,000 feet you might expect to receive a VHF signal, from a transmitter at sea level, at a range of:",
+    "options": [
+      "88.4 nm",
+      "88.4 km",
+      "70.7 km",
+      "200 km"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "In the following diagram, the phase difference between the two signals is:",
+    "options": [
+      "90\u00b0",
+      "180\u00b0",
+      "270\u00b0",
+      "360\u00b0"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "The emission characteristics A3E describe:",
+    "options": [
+      "ILS",
+      "VHF communications",
+      "HF communications",
+      "VOR"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "According to ICAO Annex 10, in which frequency band(s) does a locator normally transmit?",
+    "options": [
+      "HF/VHF",
+      "MF/HF",
+      "HF",
+      "LF/MF"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "Which Q code would give a magnetic heading to steer (nil wind) to a VDF station:",
+    "options": [
+      "QDM",
+      "QDR",
+      "QUI",
+      "QTE"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "The gain of an aerial is a measure of its ability to:",
+    "options": [
+      "Focus power",
+      "Transmit intelligence",
+      "Overcome transmitter line resistance",
+      "Compensate for attenuation"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "A frequency of 2400 kHz is equivalent to a wavelength of:",
+    "options": [
+      "125 metres",
+      "105 metres",
+      "0.80 cms",
+      "8 metres"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "The VDF Homer service provides:",
+    "options": [
+      "Accurate bearings on demand",
+      "A QGH procedure to transiting aeroplanes",
+      "Bearings which will normally be within \u00b15\u00b0 accuracy to a range of 200NM",
+      "Bearings which may be affected by synchronous transmissions"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "An aircraft wishing to use the VDF service must:",
+    "options": [
+      "Be equipped with a VOR indicator unit",
+      "Transmit a signal for a long enough period for the bearing to be established",
+      "Ask the controller to transmit for a long enough period to establish the bearing",
+      "Be within 10 NM of the VDF aerial"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "A Class C magnetic bearing is received from a station. This is:",
+    "options": [
+      "A QUI accurate to \u00b15\u00b0",
+      "A QDM accurate to \u00b110\u00b0",
+      "A QTE accurate to \u00b15\u00b0",
+      "A QDR accurate to \u00b110\u00b0"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "VOR operates in the (i), transmitting a bearing signal by means of a (ii) and uses (iii) to determine the radial:",
+    "options": [
+      "(i) VHF Stationary limacon phase comparison",
+      "(i) VHF Stationary limacon Phase comparison",
+      "(i) VHF Rotating limacon Phase comparison",
+      "(i) UHF Rotating limacon Envelope matching"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "An aircraft receives a Class A true bearing from a VDF station. This is:",
+    "options": [
+      "A QUI accurate to \u00b12\u00b0",
+      "A QTE accurate to \u00b12\u00b0",
+      "A QUI accurate to \u00b15\u00b0",
+      "A QTE accurate to \u00b15\u00b0"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "When a pilot is conducting a VDF/QGH procedure, he will require:",
+    "options": [
+      "An operator on the ground only for the VDF procedure",
+      "An operator on the ground only for the QGH procedure",
+      "An operator on the ground for both the VDF and QGH procedures",
+      "No operator on the ground for either VDF or QGH procedures"
+    ],
+    "correct": 2
+  },
+  {
+    "question": "If a ground D/F controller passes a bearing thus: \u201cYour true bearing is 256\u00b0, class alpha\u201d. This means:",
+    "options": [
+      "QTE accurate to \u00b15\u00b0",
+      "QUI accurate to \u00b13\u00b0",
+      "QDR accurate to \u00b12\u00b0",
+      "QTE accurate to \u00b12\u00b0"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "With reference to ground DF, the controller can refuse to give bearings if:",
+    "options": [
+      "The requesting aircraft is not from a consenting country",
+      "Conditions are poor and bearings do not fall within the station\u2019s classified limits",
+      "The pilot does not use the prescribed terminology",
+      "None of the above"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "Given that the compass heading is 270\u00b0, the deviation is 20\u00b0W, the variation is 30\u00b0E and the relative bearing of a beacon is 316\u00b0, determine the QDR:",
+    "options": [
+      "044",
+      "048",
+      "074",
+      "224"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "A relative bearing indicator shows 030\u00b0. The heading of the aeroplane is 090\u00b0 M. The intercept angle for a course to the NDB of 180\u00b0 M is:",
+    "options": [
+      "120\u00b0",
+      "330\u00b0",
+      "150\u00b0",
+      "060\u00b0"
+    ],
+    "correct": 3
+  },
+  {
+    "question": "An RMI shows the bearing of an NDB as 020\u00b0. The heading of the aeroplane is 020\u00b0 M. In order to intercept an outbound course of 330\u00b0 (from the NDB) at an angle of 40\u00b0, the aeroplane\u2019s heading should be altered to:",
+    "options": [
+      "010",
+      "330",
+      "300",
+      "040"
+    ],
+    "correct": 0
+  },
+  {
+    "question": "An aircraft is tracking 060\u00b0 (T) in still air. The relative bearing of an NDB is 035\u00b0 at 1300. 12 min later the relative bearing is 070\u00b0. If the G/S is 180 kt, what is the aircraft\u2019s distance from the NDB at 1312?",
+    "options": [
+      "18 nm",
+      "36 nm",
+      "24 nm",
+      "30 nm"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "To maintain the 314\u00b0 QDR inbound to a NDB with 7\u00b0 starboard drift, the heading in \u00b0M and relative bearing will be:",
+    "options": [
+      "321; 173",
+      "127; 007",
+      "141; 353",
+      "307; 183"
+    ],
+    "correct": 1
+  },
+  {
+    "question": "The indications of a VOR in an aircraft tracking towards a VOR are 075\u00b0 (M) TO and deviation indicator central. A co-located NDB shows 012\u00b0 relative. What are the drift and heading in \u00b0(M)?",
+    "options": [
+      "125; 087",
+      "122; 063",
+      "125; 063",
+      "122; 087"
+    ],
+    "correct": 3
+  }
+];
